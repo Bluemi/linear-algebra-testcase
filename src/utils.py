b4ce9e515c679eb5d7fc223ad4f9d274e37e622f
Bluemi <bruno.schilling@protonmail.ch>
2023-06-07 07:53:48 +0200

    minor improvements

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,28 @@
 import pygame as pg
+import sys
+import inspect
+import numpy as np
 
 
 def gray(b=127) -> pg.Color:
     return pg.Color(b, b, b)
 
+
 def format_float(f):
     return '{:.2f}'.format(f)
+
+
+def debug(arg):
+    """
+    Print name of arg and arg.
+    :param arg: The argument to print
+    """
+    # noinspection PyUnresolvedReferences,PyProtectedMember
+    fr = sys._getframe(1)  # type: frame
+    code = inspect.getsource(fr).split('\n')
+    line = code[fr.f_lineno - fr.f_code.co_firstlineno]
+    varname = line.partition('debug(')[2].rpartition(')')[0]
+    f_string = '{}: {}'
+    if isinstance(arg, np.ndarray):
+        f_string = '{}:\n{}'
+    print(f_string.format(varname, arg))
